0c90777801b4789a54ea14594f5812cb58bcf71b
szmahdis <szmahdis@gmail.com>
2020-08-16 16:51:05 +0200

    fix album cover in song page

--- song.js
+++ song.js
@@ -1,12 +1,14 @@
+// const paramore = './images/paramore.jpg'
+
 const songs = { //dictonary 
     'aint-it-fun': 'Paramore - Ain\'t It Fun',
     'dog-days-are-over': 'Florence + The Machine - Dog Days Are Over',
     'wasting-my-young-years': 'London Grammar - Wasting My Young Years'
 };
 const covers = {
-    'aint-it-fun': "./images/paramore.jpg",
-    'dog-days-are-over': './images/florence_and_the_machine.jpg',
-    'wasting-my-young-years': './images/london_grammar.jpg'
+    'aint-it-fun': require("./images/paramore.jpg"),
+    'dog-days-are-over': require('./images/florence_and_the_machine.jpg'),
+    'wasting-my-young-years': require('./images/london_grammar.jpg')
 }
 getSongName = () => {
     const url= window.location.href
