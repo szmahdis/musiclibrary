9055a87556085c7505001b483e7ef64352d741d9
szmahdis <szmahdis@gmail.com>
2020-08-17 15:57:15 +0200

    add light mode feature

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ for (playButton of playButtons) {
         window.location.href = `./song.html?song=${songName}`;
     }
 }
+
+const changeModeButton = document.getElementById('changeMode')
+changeModeButton.onclick = () => {
+    document.documentElement.style.setProperty('--background-color', 'white')
+    document.documentElement.style.setProperty('--primary-color', 'red')
+    document.documentElement.style.setProperty('--secondary-color', 'whitesmoke')
+    document.documentElement.style.setProperty('--text-color', 'black')
+    document.documentElement.style.setProperty('--play-button-color', 'black')
+}
